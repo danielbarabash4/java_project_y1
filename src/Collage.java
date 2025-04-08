7cf847a9b2d5e9e3a93e6dbf51f768cfbcd8fe3c
DanielLiser <ldaniel2002@gmail.com>
2025-04-08 16:18:32 +0300

    Create collage class and update lecturer class

--- src/Collage.java
+++ src/Collage.java
@@ -1,56 +1,11 @@
-//students names: Daniel Liser, Daniel Barabash
-
 import java.util.Scanner;
 
-public class Main {
+public class Collage {
+    private String name;
     static Scanner scn = new Scanner(System.in);
     private static String[] lecturers = new String[1];
     private static String[] committee = new String[1];
     private static String[] studyDepartment = new String[1];
-
-    public static void main(String[] args) {
-        System.out.println("Please Choose college name: ");
-        String collageName = scn.nextLine();
-        boolean toContinue = true;
-        char val;
-        while (toContinue) {
-            printMenu();
-            val = scn.nextLine().charAt(0);
-            switch (val) {
-                case '0':
-                    toContinue = false;
-                    break;
-                case '1':
-                    lecturers = addArrName(lecturers, "Lecture");
-                    break;
-                case '2':
-                    committee = addArrName(committee, "Committee");
-                    break;
-                case '3':
-                    studyDepartment = addArrName(studyDepartment, "Study Department");
-                    break;
-                case '4':
-                    lectureToCommittee();
-                    break;
-                case '5':
-                    //empty for now
-                    break;
-                case '6':
-                    //empty for now
-                    break;
-                case '7':
-                    System.out.println("Lecturers:");
-                    printStringArray(lecturers);
-                    break;
-                case '8':
-                    System.out.println("Committees:");
-                    printStringArray(committee);
-                    break;
-                default:
-                    System.out.println("Wrong input");
-            }
-        }
-    }
 
     private static void lectureToCommittee() {
         String lecName, comName;
@@ -58,24 +13,15 @@ public class Main {
         lecName = scn.nextLine();
         System.out.print("Choose a committee: ");
         comName = scn.nextLine();
+
+
+
         if (checkForName(lecturers, lecName)) {
             System.out.println("this lecture does not exist");
         }
         if (checkForName(committee, comName)) {
             System.out.println("this committee does not exist");
         }
-    }
-
-    private static void printMenu() {
-        System.out.println("0 - Exit");
-        System.out.println("1 - Add a lecture");
-        System.out.println("2 - Add a committee");
-        System.out.println("3 - Add a study department");
-        System.out.println("4 - Add lecture to committee");
-        System.out.println("5 - Average lecturers salary");
-        System.out.println("6 - Average lecturers salary in a department");
-        System.out.println("7 - Show the lecturers information");
-        System.out.println("8 - Show the committee");
     }
 
     private static String[] addArrName(String[] arr, String type) {
---
+++ src/Lecturer.java
@@ -0,0 +1,94 @@
+import java.util.Objects;
+
+public class Lecturer {
+        private String name;
+        private int id;
+        private String degree;
+        private String degreeName;
+        private double salary;
+        private Department department;
+        private Committee[]commitees;
+        public Lecturer(String name,int id,String degree,String degreeName,double salary,department department){
+            setId(id);
+            setName(name);
+            setDegree(degree);
+            setDegreeName(degreeName);
+            setSalary(salary);
+            setDepartment(department);
+            this.commitees=new Committee[1];
+        }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getDegreeName() {
+        return degreeName;
+    }
+
+    public void setDegreeName(String degreeName) {
+        this.degreeName = degreeName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(department department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "lecturer{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", degree='" + degree + '\'' +
+                ", degreeName='" + degreeName + '\'' +
+                ", salary=" + salary +
+                ", department=" + department +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Lecturer lecturer = (Lecturer) o;
+        return id == lecturer.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
+
+
